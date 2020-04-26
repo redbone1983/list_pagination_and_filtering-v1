e1eb99ae87f3b87d72e72d4addebe137ae0ce184
Maya Roman <maya.roman7@gmail.com>
2020-04-26 17:24:25 -0500

    Add 'wing it' version of script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
 /*** 
    Add your global variables that store the DOM elements you will 
    need to reference and/or manipulate. 
