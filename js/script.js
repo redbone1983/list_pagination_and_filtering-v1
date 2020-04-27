c96ac22382c336f2eda4e51a7ee6c7894948758b
Maya Roman <maya.roman7@gmail.com>
2020-04-27 18:31:51 -0500

    Fix anchor selection bug

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
 const studentList = document.querySelectorAll('li.student-item');
 
 const showPage = (list, page = 0) => {
@@ -12,14 +10,13 @@ const showPage = (list, page = 0) => {
   let lastItem = firstItem + 9;
   list = [...list];
 
-  for (let item in list) {
+for (let item in list) {
     item = Number(item);
     if (item >= firstItem && item <= lastItem) {
       list[item].style.display = "";
     } else {
       list[item].style.display = "none";
     }
-    
   }
 };
 
@@ -34,41 +31,44 @@ const appendPageLinks = (list) => {
   const ul = document.createElement('ul');
   navDiv.className = 'pagination';
   
+  let pages;
+
   pageDiv.appendChild(navDiv);
   navDiv.appendChild(ul);
 
-  
-  let pages;
-
   for (let i = 1; i <= numOfPages; i += 1) {
     const listItem = document.createElement('li');
-    pages = document.querySelectorAll('a');
-    pages = [...pages];
     const anchorLink = document.createElement('a');
     anchorLink.textContent = i;
     anchorLink.href = "#";
-
-    
     listItem.appendChild(anchorLink);
     ul.appendChild(listItem);
 
+    pages = document.querySelectorAll('a');
+    pages = [...pages];
+
+
+
     anchorLink.addEventListener('click', (event) => {
       const pageNum = Number(event.target.textContent);
-      
-      for (let anchor in pages) {
-        if (pages[anchor].className === 'active') {
-          pages[anchor].className = 'inactive';
-        } 
+      for (let link in pages) {
+        pages[link].className = 'inactive';
       }
-      
+
+
       event.target.className = 'active';
+      
       if (pageNum === 1) {
         showPage(list, 0);
       } else {
         showPage(list, pageNum);
       }
+
+
     });
   }
+
+
 };
 
 appendPageLinks(studentList);
--- js/wing_it.js
+++ js/wing_it.js
@@ -2,40 +2,10 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const studentList = document.querySelectorAll('li.student-item');
 const body = document.querySelector('body');
 const div = document.querySelector('div');
 let linkDiv = document.createElement('div');
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-     Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
 
 const showPage = (list, page) => {
   list = [...list];
@@ -62,17 +32,11 @@ const showPage = (list, page) => {
   }
 };
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
 const appendPageLinks = (list) => {
   let newLinkButton;
   list = [...list];
   let numOfButtons = list.length / 10;
   numOfButtons = Math.round(numOfButtons);
-
   for (let i = 1; i <= numOfButtons; i += 1) {
     newLinkButton = document.createElement('button');
     newLinkButton.textContent = i;
@@ -82,9 +46,7 @@ const appendPageLinks = (list) => {
     linkDiv.style.paddingTop = '10px';
     div.appendChild(linkDiv);
   }
-
   body.appendChild(div);
-
   // Set Default view to page 1
   showPage(studentList, 0);
 };
@@ -95,26 +57,9 @@ linkDiv.addEventListener('click', (event) => {
   let pageButton = event.target;
   let pageNum = pageButton.textContent;
   pageNum = Number(pageNum);
-        
   if (pageButton.tagName === 'BUTTON' && pageNum !== 1) {
     showPage(studentList, pageNum);
   } else {
     showPage(studentList, 0);
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
