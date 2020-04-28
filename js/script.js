3d1108a9a60f128b0cca2886c0d09a9f03d4814c
Maya Roman <maya.roman7@gmail.com>
2020-04-28 13:54:39 -0500

    Add search button to DOM

--- js/script.js
+++ js/script.js
@@ -4,6 +4,21 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
    
 const studentList = document.querySelectorAll('li.student-item');
+const pageHeader = document.querySelector('.page-header');
+const searchDiv = document.createElement('div');
+const searchInput = document.createElement('input');
+const searchButton = document.createElement('button');
+
+
+// append search to DOM
+pageHeader.appendChild(searchDiv);
+searchDiv.appendChild(searchInput);
+searchDiv.appendChild(searchButton);
+
+searchDiv.className = 'student-search';
+searchInput.placeholder = 'Search for students...';
+searchButton.textContent = 'search';
+
 
 const showPage = (list, page = 0) => {
   let firstItem = page * 10;
@@ -47,28 +62,19 @@ const appendPageLinks = (list) => {
     pages = document.querySelectorAll('a');
     pages = [...pages];
 
-
-
-    anchorLink.addEventListener('click', (event) => {
+      anchorLink.addEventListener('click', (event) => {
       const pageNum = Number(event.target.textContent);
       for (let link in pages) {
         pages[link].className = 'inactive';
       }
-
-
       event.target.className = 'active';
-      
       if (pageNum === 1) {
         showPage(list, 0);
       } else {
         showPage(list, pageNum);
       }
-
-
     });
   }
-
-
 };
 
 appendPageLinks(studentList);
