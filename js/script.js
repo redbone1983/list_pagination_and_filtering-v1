51a1bd8eadccf993d8d4d9d07b1fec623c73be70
Maya Roman <maya.roman7@gmail.com>
2020-05-13 17:49:09 -0500

    Tidy up and minimize global variable use

--- js/script.js
+++ js/script.js
@@ -5,62 +5,9 @@ FSJS project 2 - List Filter and Pagination
 
 // Select Main DOM elements
 const itemsPerPage = 10;
-const pageDiv = document.querySelector('.page');
-const pageHeader = document.querySelector('.page-header');
-const studentUl = document.querySelector('.student-list');
 const studentLi = document.querySelectorAll('.student-item');
-const studentNames = document.querySelectorAll('h3');
-
-// Add search components
-const searchDiv = document.createElement('div');
-const searchInput = document.createElement('input');
-const searchButton = document.createElement('button');
-const keyupEventButton = document.createElement('button');
-const resetButton = document.createElement('button');
-
-searchDiv.className = 'student-search';
-searchInput.type = 'text';
-searchInput.placeholder = 'search for students...';
-searchButton.textContent = 'click search';
-keyupEventButton.textContent = 'keyup search';
-resetButton.textContent = 'reset';
-
-// Append Search Elements to the DOM
-pageHeader.appendChild(searchDiv);
-searchDiv.appendChild(searchButton);
-searchDiv.appendChild(keyupEventButton);
-
-// Initializes global boolean to false so search feature can be set by user
 let runClick = false;
 let runKeyup = false;
-
-// ** Adds EventListener to buttons to activate 'exceeds' features **
-
-// Button to search with click feature
-searchButton.addEventListener('click', (event) =>  {
-  runClick = true;
-  searchDiv.appendChild(searchInput);
-  keyupEventButton.style.display = 'none';
-  event.target.textContent = 'search';
-  searchDiv.appendChild(searchButton);
-  searchDiv.appendChild(resetButton);
-});
- 
-// Button to search with keyup feature
-keyupEventButton.addEventListener('click', () => {
-  runKeyup = true;
-  searchInput.placeholder = 'Enter text to search...';
-  searchDiv.appendChild(searchInput);
-  searchButton.style.display = 'none';
-  keyupEventButton.style.display = 'none';
-  searchDiv.appendChild(resetButton);
-});
-
-// Reloads page
-resetButton.addEventListener('click', () => {
-  location.reload();
-  return false;
-});
 
 // Shows 10 list items per selected page
 const showPage = (list, page) => {
@@ -77,30 +24,13 @@ for (let item in list) {
   }
 };
 
-// Creates and appends element to store 'No Results' message
-let message = document.createElement('p');
-message.style.padding = '10px';
-message.style.display = 'none';
-pageDiv.insertBefore(message, studentUl);
-
-
-// Handles no results returned
-const notFoundMessage = (name) => {
-  message.textContent = `${name} is not found in this directory.`;
-  message.style.display = '';
-};
-
-// Sets navDiv globally so page links can reset after each search
-let navDiv;
-
 const appendPageLinks = list => {
   let numOfPages = Math.ceil(list.length / 10);
-  
-  // Create Page Link DOM elements
-  navDiv = document.createElement('div');
-  const navUl = document.createElement('ul');
+  let pageDiv = document.querySelector('.page');
+  const navDiv = document.createElement('div');
   navDiv.className = 'pagination';
-    
+  const navUl = document.createElement('ul');
+  
   // Append Page Link DOM elements
   pageDiv.appendChild(navDiv);
   navDiv.appendChild(navUl);
@@ -130,7 +60,7 @@ const appendPageLinks = list => {
 
 // Removes page links in order to avoid appending duplicates to the DOM
 const removePageLinks = () => {
-  pageDiv.removeChild(navDiv);
+  document.querySelector('.page').removeChild(document.querySelector('div .pagination'));
 };
 
 // Shows default page view
@@ -139,49 +69,119 @@ showPage(studentLi, 1);
 // Adds default page links
 appendPageLinks(studentLi);
 
-// ** Adds functionality to the search component **
+const createErrorMsg = () => {
+  const studentUl = studentLi[0].parentNode;
+  const pageDiv = studentUl.parentNode;
+  let message = document.createElement('p');
+  message.style.padding = '10px';
+  message.style.display = 'none';
+  pageDiv.insertBefore(message, studentUl);
+};
 
-// Search Event Handler
-const runSearchEvent = () => {
-  let searchResults = [];
+createErrorMsg();
+
+const showErrorMsg = (name) => {
+  let message = document.querySelector('p');
+  message.textContent = `${name} is not found in this directory.`;
+  message.style.display = '';
+};
+
+const removeErrorMsg = () => {
+  let message = document.querySelector('p');
+  message.style.display = 'none';
+};
+
+const runSearch = (searchInput, list) => {
+  removePageLinks();
+  const searchResults = [];
+  const studentNames = document.querySelectorAll('h3');
   let names = [...studentNames];
+
   for (let i = 0; i < names.length; i += 1) {
     if (!names[i].textContent.includes(searchInput.value.toLowerCase())) {
-      studentLi[i].style.display = 'none';
+      list[i].style.display = 'none';
     } else {
-    // Paginate search results
-      searchResults.push(studentLi[i]);
+      searchResults.push(list[i]);
     } 
   }
 
-  // Removes default page links
-  removePageLinks();
-  
   // Updates browser view with searchResults
   showPage(searchResults, 1);
   
   // Updates pageLinks with searchResults
   appendPageLinks(searchResults);
-   
-  // Displays 'No Results' message
-  if (searchResults.length === 0) {
-    notFoundMessage(searchInput.value);
-  } 
 
-  // Removes 'No Results' message if searchInput is empty
+  // Check Results 
+
+  if (searchResults.length === 0) {
+    showErrorMsg(searchInput.value);
+  }
+
   if (searchInput.value.length === 0 || searchResults.length > 0) {
-    message.style.display = 'none';
+    removeErrorMsg();
   }
 };
 
-searchInput.addEventListener('keyup', () => {
-  if (runKeyup) {
-    runSearchEvent();
-  } 
-});
+const addSearchBar = () => {
+  const pageHeader = document.querySelector('.page-header');
+  const searchDiv = document.createElement('div');
+  const searchInput = document.createElement('input');
+  searchDiv.className = 'student-search';
+  searchInput.type = 'text';
+  searchInput.placeholder = 'search for students...';
+  
+  // add search button set
+  const clickSearchButton = document.createElement('button');
+  const keyupEventButton = document.createElement('button');
+  const resetButton = document.createElement('button');
+  clickSearchButton.textContent = 'click search';
+  keyupEventButton.textContent = 'keyup search';
+  resetButton.textContent = 'reset';
 
-searchButton.addEventListener('click', () => {
-  if (runClick) {
-    runSearchEvent();
-  } 
-}); 
+  // Append Search Elements to the DOM
+  pageHeader.appendChild(searchDiv);
+  searchDiv.appendChild(clickSearchButton);
+  searchDiv.appendChild(keyupEventButton);
+
+  // Button to search with click feature
+  clickSearchButton.addEventListener('click', () =>  {
+    runClick = true;
+    searchDiv.appendChild(searchInput);
+    keyupEventButton.style.display = 'none';
+    clickSearchButton.textContent = 'search';
+    searchDiv.appendChild(clickSearchButton);
+    searchDiv.appendChild(resetButton);
+  });
+ 
+  // Button to search with keyup feature
+  keyupEventButton.addEventListener('click', () => {
+    runKeyup = true;
+    searchInput.placeholder = 'Enter text to search...';
+    searchDiv.appendChild(searchInput);
+    clickSearchButton.style.display = 'none';
+    keyupEventButton.style.display = 'none';
+    searchDiv.appendChild(resetButton);
+  });
+
+  // Reloads page
+  resetButton.addEventListener('click', () => {
+    location.reload();
+    return false;
+  });
+
+  // Runs keyup search
+  searchInput.addEventListener('keyup', (event) => {
+    if (runKeyup) {
+      runSearch(event.target, studentLi);
+    } 
+  });
+  
+  // Runs click search
+  clickSearchButton.addEventListener('click', () => {
+    if (runClick) {
+      runSearch(searchInput, studentLi);
+    } 
+  });
+};
+
+addSearchBar();
